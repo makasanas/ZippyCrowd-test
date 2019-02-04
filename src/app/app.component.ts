f6e9760242eebf1599a880c5c23faa44e37d726b
Sanjay Makasana <sanjaymakasana@Sanjays-MacBook-Air-2.local>
2019-02-04 13:20:03 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ZippyCrowd2';
+
+  public getRecommendations:any;
+  public initialSkills = ['Beautician', 'Coiffeur', 'Coiffeuse', 'Cosmetologist', 'Hair stylist', 'Hairdresser'];
+  public isSkillSelected:boolean = false;
+  public isSkillChange:boolean= false;
+  public tempLocation = '';
+
+  constructor(private formBuilder: FormBuilder) {
+		this.getRecommendations = this.formBuilder.group({
+      'skill': [''],
+      'location': [''],
+      'isLocation':false
+		});
+  }
+
+
+  locationOnChange(){
+    if(this.getRecommendations.get('isLocation').value){
+      this.tempLocation = this.getRecommendations.get('location').value;
+      this.getRecommendations.get('location').setValue('');
+    }else{
+      this.getRecommendations.get('location').setValue(this.tempLocation);
+    }
+  }
+
+  selectSkill(skill){
+    this.getRecommendations.get('skill').setValue(skill);
+    this.isSkillSelected = true;
+    this.isSkillChange = true;
+  }
+  
+  getRecommendation(){
+    if(this.isSkillSelected){
+      console.log(this.getRecommendations.value);
+      this.getRecommendations.reset({'skill':''});
+      this.isSkillSelected = false;
+      this.isSkillChange = false;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+//pipes
+import { FilterPipe} from './pipes/filter.pipe';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
